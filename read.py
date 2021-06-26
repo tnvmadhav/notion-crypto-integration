7cc199e1af5b5cbded40bd5a3663e93289da84ce
T Venu Madhav <tnvmadhav@gmail.com>
2021-06-26 16:54:18 +0530

    Add required files

---
+++ read.py
@@ -0,0 +1,81 @@
+import json
+import time
+
+import requests
+import yaml
+
+
+class MyIntegration:
+
+    def __init__(self):
+        """
+        Gets required variable data from config yaml file.
+        """
+        with open("my_variables.yml", 'r') as stream:
+            try:
+                self.my_variables_map = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print("[Error]: while reading yml file", exc)
+
+    def getCryptoPrices(self):
+        """
+        Download the required crypto prices using Binance API.
+        Ref: https://github.com/binance/binance-api-postman
+        """
+        for name, data in self.my_variables_map["NOTION_ENTRIES"].items():
+            url = f"https://api.binance.com/api/v3/avgPrice?"\
+                f"symbol={name}{self.my_variables_map['REFERENCE_CURRENCY']}"
+            payload = {}
+            headers = {}
+            response = requests.request(
+                "GET", url, headers=headers, data=payload
+            )
+            if response.status_code == 200:
+                content = response.json()
+                data['price'] = content['price']
+
+    def updateNotionDatabase(self, pageId, coinPrice):
+        """
+        A notion database (if integration is enabled) page with id `pageId`
+        will be updated with the data `coinPrice`.
+        """
+        url = "https://api.notion.com/v1/pages/" + str(pageId)
+        headers = {
+            'Authorization':
+                'Bearer ' + self.my_variables_map["MY_NOTION_SECRET_TOKEN"],
+            'Content-Type': 'application/json'
+        }
+        payload = json.dumps({
+            "properties": {
+                "Price/Coin": {
+                    "type": "number",
+                    "number": float(coinPrice),
+                },
+            }
+        })
+        print(requests.request(
+                "PATCH", url, headers=headers, data=payload
+            ).text)
+
+    def UpdateIndefinitely(self):
+        """
+        Orchestrates downloading prices and updating the same
+        in notion database.
+        """
+        while True:
+            try:
+                self.getCryptoPrices()
+                for _, data in self.my_variables_map["NOTION_ENTRIES"].items():
+                    self.updateNotionDatabase(
+                        pageId=data['page'],
+                        coinPrice=data['price'],
+                    )
+                    time.sleep(2 * 10)
+                time.sleep(1 * 60)
+            except Exception as e:
+                print(f"[Error encountered]: {e}")
+
+
+if __name__ == "__main__":
+    # With 😴 sleeps to prevent rate limit from kicking in.
+    MyIntegration().UpdateIndefinitely()
